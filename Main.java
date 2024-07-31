d0ae28ee7bbc1add6bcdad2ccbbaf37f6c06d3e5
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-07-31 15:33:34 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+ 
+public class Main {
+  
+    public static void main(String[] args) {
+        Person person = new Person("Alex Sison", "Filipino", "1986/01/17", 7);
+ 
+      System.out.println("Name: " + person.name + "\n" + "Nationality: " + person.nationality + "\n" + "Date of Birth: " + person.dateOfBirth + "\n" + "Seat Number: " + person.seatNumber);
+
+    }
+  
+  
+}
